e185648681d31417d0310bd5903310ccb02a2561
guim0 <guimodev@gmail.com>
2024-02-02 10:20:08 -0300

    :sparkles: feat: Add new photo

--- src/page/Home/index.tsx
+++ src/page/Home/index.tsx
@@ -3,7 +3,7 @@ import styles from "./Home.module.scss";
 export const Home = () => {
   return (
     <main className={styles.home}>
-      <h1>Hi, I'm Guilherme</h1>
+      <h1>Hello, my name is Guilherme but i prefer Guimo</h1>
       <section>
         <p>
           As a Front-end developer, i always wonder how can i improve in my
